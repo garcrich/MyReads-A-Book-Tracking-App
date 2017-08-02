1f20e107fd02e9facecd4d8e3a117888c309eaed
Ricky Garcia <rg3646@outlook.com>
2017-08-01 23:25:55 -0600

    imported BooksAPI into search.js, added state and serachQuery method to component, not commented to BooksAPI yet

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
 
 class SearchBook extends Component {
+    state = { searchedBooks : '' }
+
+    searchQuery = (event) => {
+        this.setState({searchedBooks: event.target.value})
+    }
 
     render() {
         return <div className="search-books">
@@ -16,16 +22,8 @@ class SearchBook extends Component {
 
 
                 <div className="search-books-input-wrapper">
-                    {
-                    /* 
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                    
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                    <input type="text" placeholder="Search by title or author" />
+                    <input type="text" value={this.state.searchedBooks} onChange={this.searchQuery} placeholder="Search by title or author" />
+                    {this.state.searchedBooks}
 
                 </div>
             </div>
