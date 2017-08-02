9603848488c3a067c06559bd05d6fad70a54151b
Ricky Garcia <rg3646@outlook.com>
2017-08-01 22:28:17 -0600

    refactored css for organization

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import SearchBook from './components/search'
-import './App.css'
+import './css/App.css'
 
 class App extends React.Component {
 
   render() {
-    
+
     return (
       <div className="app">
         <Route path="/search" render={() => (
           <SearchBook />
         )} />
-
 
         <Route path="/" exact render={() => (
           <HomePage />
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 //import BookShelf from './BookShelf'
-import './index.css'
+import './css/index.css'
 
 /* <BrowserRouter><App /></BrowserRouter> */
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
