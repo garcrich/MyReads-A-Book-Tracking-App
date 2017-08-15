5ca9571c5b000d79490d25b96050a3a89ba80e57
Ricky Garcia <rg3646@outlook.com>
2017-08-14 22:43:19 -0600

    removed css logic as it is not needed

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -9,17 +9,17 @@ class BookShelf extends Component {
             <div className="bookshelf-books">
                 <ol className="books-grid">
                     {Array.isArray(this.props.books) && (
-                        this.props.books.map((book, index) =>
-                        <li key={index}>
+                        this.props.books.map((book) =>
+                        <li key={book.id}>
                             <div className="book">
                                 <div className="book-top">
                                     <div className="book-cover" style={{ width: 128, height: 170, backgroundImage: book.imageLinks ? (`url(${book.imageLinks.thumbnail})`) : (`url(https://dummyimage.com/128x170/4f4f4f/ffffff.jpg&text=No+Book+Art)`) }}></div>
                                     <div className="book-shelf-changer">
                                         <select value={book.shelf} onChange={(event) => this.props.updateShelf(book, event.target.value)}> 
                                             <option disabled>Move to...</option>
-                                            <option className={(book.id === this.props.matchingBooks) ? (("currentlyReading" === this.props.matchingShelf) ? 'shelfHighlight' : '') : ''} value="currentlyReading">Currently Reading</option>
-                                            <option className={(book.id === this.props.matchingBooks) ? (("wantToRead" === this.props.matchingShelf) ? 'shelfHighlight' : '') : ''} value="wantToRead">Want to Read</option>
-                                            <option className={(book.id === this.props.matchingBooks) ? (("read" === this.props.matchingShelf) ? 'shelfHighlight' : '') : ''} value="read">Read</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
                                             <option value="none">None</option>
                                         </select>
                                     </div>
